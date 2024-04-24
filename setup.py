cf63276f70edfb8faebcd89a2c158a85a7262382
puyuan1996 <2402552459@qq.com>
2024-04-24 17:38:01 +0800

    sync code

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(name='reasoners',
                         'numpy',
                         'scipy',
                         'torch',
-                        'datasets',
+                        # 'datasets',
                         'huggingface_hub',
                         'transformers',
                         'sentencepiece',
@@ -23,12 +23,14 @@ setup(name='reasoners',
                         'optimum',
                         'ninja',
                         'bitsandbytes',
-                        'llama1@git+https://github.com/AegeanYan/llama@llama_v1',  # llama 2 have some alias problem so you may need to clone the forked llama1 at private repo. please check the setup.py
-                        'llama@git+https://github.com/facebookresearch/llama@main',
+                        # 'llama1@git+https://github.com/AegeanYan/llama@llama_v1',  # llama 2 have some alias problem so you may need to clone the forked llama1 at private repo. please check the setup.py
+                        # 'llama@git+https://github.com/facebookresearch/llama@main',
                         'fairscale'],
       include_package_data=True,
       extras_require={
           'awq': ['awq@git+https://github.com/mit-han-lab/llm-awq',
                   'awq_inference_engine@git+https://github.com/mit-han-lab/llm-awq.git@main#subdirectory=awq/kernels'],
       },
-      python_requires='>=3.10')
+      python_requires='>=3.9')
+    #   python_requires='>=3.10')
+
